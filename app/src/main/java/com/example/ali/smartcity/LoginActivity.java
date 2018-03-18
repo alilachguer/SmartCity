dd8c6e02f4dd4dd3bf53e4884de614e72f754e1f
alilachguer <alilachguer95@gmail.com>
2018-03-18 20:54:00 +0100

    add icon to the app

--- app/src/main/java/com/example/ali/smartcity/LoginActivity.java
+++ app/src/main/java/com/example/ali/smartcity/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.example.ali.smartcity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,11 +10,13 @@ import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText username, password;
     Button login;
+    TextView registerLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +26,8 @@ public class LoginActivity extends AppCompatActivity {
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        registerLink = (TextView) findViewById(R.id.register_link);
+
 
     }
 
